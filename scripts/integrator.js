7c1b8443300d3e7ad083cbccd56590262a4210ee
Mikhail Dudin <dudin-m@live.ru>
2014-04-19 19:38:56 +0400

    Небольшие изменения в Интеграторе

--- scripts/integrator.js
+++ scripts/integrator.js
@@ -5,65 +5,85 @@
 function Integrator( initialN ) {
 	var objFunc;
 	var delta; //погрешность
-	var intervalA, intervalB;
-	var n = initialN, h;
-	var maxN = -Infinity;
-	
+	var integrIntervalA, integrIntervalB;
+	var maxAppropriateN = -Infinity;
+
+	this.getIntegralValue = function() {
+		return countIntegralValue();
+	}
+
 	/**
-	* Функция для подсчета значения интеграла
-	* @returns Значение интеграла
-	*/
-	this.getValue = function() {				
-		var resultWithN = -Infinity,
-			resultWith2N = +Infinity;
-			
-		while( Math.abs( resultWithN - resultWith2N ) > delta ) {
-			var h1 = (intervalB - intervalA) / n,
-				h2 = (intervalB - intervalA) / (2*n);
-			
-			resultWithN = rectFormula( n );
-			resultWith2N = rectFormula( 2*n );
-			
-			console.log("result:\t" + resultWithN + "\t" + resultWith2N);
-			
+	 * Функция для подсчета значения интеграла с учетом требуемой точности по заданному отрезку
+	 * @returns {number} Значение интеграла
+	 */
+	function countIntegralValue() {
+		var resultForN = -Infinity,
+			resultFor2N = +Infinity;
+		var appropriateN; //число разбиений, реализующих заданную точность
+		var n = initialN;
+
+		while( Math.abs( resultForN - resultFor2N ) > delta ) {
+			resultForN = rectFormula( n );
+			resultFor2N = rectFormula( 2*n );
+
+			appropriateN = n;
 			n++;
 		}
-		
-		console.log(n-1);
-		console.log("result:\t" + resultWithN + "\t" + resultWith2N);
-		return resultWithN;
+
+		findMaxAppropriateN( appropriateN );
+
+		return resultForN;
 	}
-	
-	function rectFormula( num ) {
-		var h = (intervalB - intervalA) / num;
+
+	/**
+	 * Функция для вычисления интеграла по заданному разбиению
+	 * @param n Разбиение
+	 * @returns {number} Значение интеграла от integrIntervalA до integrIntervalB
+	 */
+	function rectFormula( n ) {
+		var h = (integrIntervalB - integrIntervalA) / n;
 		var result = 0,
-			x, 
-			prevX = intervalA;
-			
-		for( var i = 0; i < num; i++ ) {
+			x,
+			prevX = integrIntervalA;
+
+		for( var i = 0; i < n; i++ ) {
 			x = prevX + h;
 			var middle = ( x + prevX ) / 2;
 			result += h * objFunc.getValue( middle );
 			prevX = x;
 		}
-		
+
 		return result;
 	}
-	
+
+	/**
+	 * Функция для нахождения максимума из всех минимальных допустимых разбиений
+	 * @param n Текущее подходящее разбиение
+	 */
+	function findMaxAppropriateN( n ) {
+		if( n >= maxAppropriateN )
+			maxAppropriateN = n;
+		return;
+	}
+
+	this.getMaxAppropriateN = function() {
+		return maxAppropriateN;
+	}
+
 	this.setObjFunc = function( f ) {
 		objFunc = f;
 	};
-	
+
 	this.setDelta = function( d ) {
 		delta = d;
 	};
-	
-	this.setIntgrInterval = function( A, B ) {
-		intervalA = A;
-		intervalB = B;
+
+	this.setIntegrInterval = function( A, B ) {
+		integrIntervalA = A;
+		integrIntervalB = B;
 	};
-	
-	this.setN = function( _n ) {
-		n = _n;
+
+	this.setN = function( n ) {
+		initialN = n;
 	};
 }
